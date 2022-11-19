b2eb7b7fe6af2aec94b692734dd3bb2c6140a982
LailatulHasanah31 <117157462+LailatulHasanah31@users.noreply.github.com>
2022-11-19 16:37:59 +0700

    Input View

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        // return view('welcome_message');
+        return view('pages/index.html');
+    }
+
+    public function HomeAdminStaff()
+    {
+        // return view('welcome_message');
+        return view('pages/HomeAdminStaff.html');
+    }
+
+    public function HomeKepala()
+    {
+        // return view('welcome_message');
+        return view('pages/HomeKepala.html');
+    }
+
+    public function layananSKM()
+    {
+        // return view('welcome_message');
+        return view('pages/layananSKM.html');
+    }
+
+    public function login()
+    {
+        // return view('welcome_message');
+        return view('pages/login.html');
+    }
+
+    public function pengajuanSKM()
+    {
+        // return view('welcome_message');
+        return view('pages/pengajuanSKM.html');
+    }
+
+    public function profil()
+    {
+        // return view('welcome_message');
+        return view('pages/profil.html');
+    }
+
+    public function SkmAdminStaff()
+    {
+        // return view('welcome_message');
+        return view('pages/SkmAdminStaff.html');
+    }
+
+    public function SkmKepala()
+    {
+        // return view('welcome_message');
+        return view('pages/SkmKepala.html');
+    }
+
+    public function TTDKepala()
+    {
+        // return view('welcome_message');
+        return view('pages/TTD kepala.html');
+    }
+}
